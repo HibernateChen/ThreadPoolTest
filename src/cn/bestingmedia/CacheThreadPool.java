cfdf60e405bcebb6a6a51c65c49ca475b7231470
chen <18583241813@163.com>
2019-05-29 16:42:34 +0800

    第四次修改

--- src/cn/bestingmedia/CacheThreadPool.java
+++ src/cn/bestingmedia/CacheThreadPool.java
@@ -7,7 +7,9 @@ public class CacheThreadPool {  //试试修改提交会不会覆盖
 	
 	public static void main(String[] args) {
 		ExecutorService service = Executors.newCachedThreadPool();
-		 
+		
+		int a =0;
+		
 		for (int i = 0; i < 100; i++) {
 			service.execute(()->{ //lambel表达式
 				try {
